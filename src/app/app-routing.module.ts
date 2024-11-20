6841e894910e10a776cf6ad55b10db57503f5cee
Assmae <alidaassmae@gmail.com>
2024-11-20 11:37:46 +0100

    ajout header et login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { FoodPageComponent } from './food-page/food-page.component';
 import {HomeComponent} from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
 const routes: Routes = [
   {path:'', component:HomeComponent},
   {path:'search/:searchTerm', component:HomeComponent},
   {path:'tag/:tag', component:HomeComponent},
   {path:'food/:id', component:FoodPageComponent},
-  {path:'cart-page', component: CartPageComponent}
+  {path:'cart-page', component: CartPageComponent},
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
